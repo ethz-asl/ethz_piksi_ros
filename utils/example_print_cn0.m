d59809366d590538fa650bb0fb44a19e3b0462a5
marco-tranzatto <femer90@hotmail.it>
2017-06-08 18:41:27 +0200

    correct incorrect csv name

--- utils/example_print_cn0.m
+++ utils/example_print_cn0.m
@@ -1,5 +1,5 @@
 %% load cvs data extracted from rosbag
-data = load('debug_state_2017_06_08_14_39_59');
+data = load('gps_helix_antenna');
 
 %% plot carrier to nois ratio
 [cn0, relative_time] = extract_cn0(data);
