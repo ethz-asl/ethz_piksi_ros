b9393fa42ce3c6cf8b1a71a28086d64e405e72c5
Rik Bähnemann <brik@ethz.ch>
2018-10-19 22:09:39 +0200

    Switch SBP version.

--- piksi_multi_rtk_ros/src/piksi_multi_rtk_ros/piksi_multi.py
+++ piksi_multi_rtk_ros/src/piksi_multi_rtk_ros/piksi_multi.py
@@ -51,7 +51,7 @@ import collections
 
 
 class PiksiMulti:
-    LIB_SBP_VERSION_MULTI = '2.3.15'  # SBP version used for Piksi Multi.
+    LIB_SBP_VERSION_MULTI = '2.1.14'  # SBP version used for Piksi Multi.
 
     # Geodetic Constants.
     kSemimajorAxis = 6378137
