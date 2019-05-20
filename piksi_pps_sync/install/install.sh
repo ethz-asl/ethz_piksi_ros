b6a25f87ee837c3b71900812cb9d74e541adbf71
Rik Bähnemann <brik@ethz.ch>
2019-05-20 16:48:37 +0200

    More installation.

--- piksi_pps_sync/install/install.sh
+++ piksi_pps_sync/install/install.sh
@@ -13,11 +13,26 @@ sudo cp pps-gpio-modprobe.ko /lib/modules/$(uname -r)/kernel/drivers/pps/clients
 sudo depmod
 sudo sh -c 'echo "pps-gpio-modprobe" >> /etc/modules-load.d/10-pps-gpio-modprobe.conf'
 sudo sh -c "echo 'options pps-gpio-modprobe gpio=${GPIO_PIN}' >> /etc/modprobe.d/10-pps-gpio-modprobe.conf"
+cd ..
 
 # Install NMEA GPS
+sudo apt install gpsd -y
+sudo cp cfg/gpsd /etc/defaults/
+sudo dpkg-reconfigure gpsd
+
 sudo sh -c "echo '#!/bin/bash' >> /etc/rc.local"
 sudo sh -c "echo '' >> /etc/rc.local"
 sudo sh -c "echo '# Setting GPS UART' >> /etc/rc.local"
 sudo sh -c "echo 'stty -F ${DEVICE} ${BAUD}' >> /etc/rc.local"
+sudo sh -c "echo '# Start GPSD' >> /etc/rc.local"
+sudo sh -c "echo 'service gpsd start' >> /etc/rc.local"
 sudo sh -c "echo '' >> /etc/rc.local"
 sudo sh -c "echo 'exit 0' >> /etc/rc.local"
+
+# Install chrony.
+sudo apt install chrony -y
+
+sudo sh -c "echo '' >> /etc/chrony/chrony.conf"
+sudo sh -c "echo '# GPS + PPS' >> /etc/chrony/chrony.conf"
+sudo sh -c "echo 'refclock PPS /dev/pps0 lock NMEA' >> /etc/chrony/chrony.conf"
+sudo sh -c "echo 'refclock SHM 0 refid NMEA' >> /etc/chrony/chrony.conf"
