12f4d93fb2e1aac0a5d72a777220a057a03d156b
Rik Bähnemann <brik@ethz.ch>
2019-08-19 13:12:05 +0200

    Use tee to append to file configurations.

--- piksi_pps_sync/install/install.sh
+++ piksi_pps_sync/install/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Please enter the PPS GPIO pin number, e.g., 250?"
+echo "Please enter the PPS GPIO pin number, e.g., 250..."
 read GPIO_PIN
 echo "PPS is triggered on pin ${GPIO_PIN}."
 
-echo "Please enter the NMEA UART device, e.g., /dev/ttyS5?"
+echo "Please enter the NMEA UART device, e.g., /dev/ttyS5..."
 read DEVICE
 echo "Using serial port ${DEVICE}."
 
-echo "Please enter the serial port baud rate, e.g., 115200?"
+echo "Please enter the serial port baud rate, e.g., 115200..."
 read BAUD
 echo "Setting baud rate to ${BAUD}."
 
@@ -23,24 +23,23 @@ cd ..
 
 # Install NMEA GPS
 sudo apt install gpsd -y
-sudo cp cfg/gpsd /etc/default/gpsd
 echo "Do you wish to configure gpsd? [y or Y to accept]"
 read configure_gpsd
 if [[ $configure_gpsd == "Y" || $configure_gpsd == "y" ]]; then
   echo "Configuring /etc/default/gpsd"
   sudo rm /etc/default/gpsd
-  sudo sh -c "echo '# Start the gpsd daemon automatically at boot time.' >> /etc/default/gpsd"
-  sudo sh -c "echo 'START_DAEMON=\"true\"' >> /etc/default/gpsd"
-  sudo sh -c "echo '# Use USB hotplugging to add new USB devices automatically to the daemon.' >> /etc/default/gpsd"
-  sudo sh -c "echo 'USBAUTO=\"false\"' >> /etc/default/gpsd"
-  sudo sh -c "echo '# Devices gpsd should collect to at boot time.' >> /etc/default/gpsd"
-  sudo sh -c "echo '# They need to be read/writeable, either by user gpsd or the group dialout.' >> /etc/default/gpsd"
-  sudo sh -c "echo 'DEVICES=\"${DEVICE}\"' >> /etc/default/gpsd"
-  sudo sh -c "echo '# Other options you want to pass to gpsd.' >> /etc/default/gpsd"
-  sudo sh -c "echo 'GPSD_OPTIONS=\"-n -r\"' >> /etc/default/gpsd"
-  sudo sh -c "echo '' >> /etc/default/gpsd"
-  sudo sh -c "echo 'GPSD_SOCKET=\"/var/run/gpsd.sock\"' >> /etc/default/gpsd"
-  sudo chmod +x /etc/rc.local
+  sudo sh -c "tee -a /etc/default/gpsd << END
+# Start the gpsd daemon automatically at boot time.
+START_DAEMON=\"true\"
+# Use USB hotplugging to add new USB devices automatically to the daemon.
+USBAUTO=\"false\"
+# Devices gpsd should collect to at boot time.
+# They need to be read/writeable, either by user gpsd or the group dialout.
+DEVICES=\"${DEVICE}\"
+# Other options you want to pass to gpsd.
+GPSD_OPTIONS=\"-n -r\"
+GPSD_SOCKET=\"/var/run/gpsd.sock\"
+END"
 fi
 
 sudo dpkg-reconfigure gpsd
@@ -50,17 +49,18 @@ read create_rc_local
 if [[ $create_rc_local == "Y" || $create_rc_local == "y" ]]; then
   echo "Creating /etc/rc.local"
   sudo rm /etc/rc.local
-  sudo sh -c "echo '#!/bin/bash' >> /etc/rc.local"
-  sudo sh -c "echo '' >> /etc/rc.local"
-  sudo sh -c "echo '# Start pps-gpio-modprobe module.' >> /etc/rc.local"
-  sudo sh -c "echo 'modprobe pps-gpio-modprobe gpio=${GPIO_PIN}' >> /etc/rc.local"
-  sudo sh -c "echo '# Setting GPS UART' >> /etc/rc.local"
-  sudo sh -c "echo 'service gpsd stop' >> /etc/rc.local"
-  sudo sh -c "echo 'stty -F ${DEVICE} ${BAUD}' >> /etc/rc.local"
-  sudo sh -c "echo '# Start GPSD' >> /etc/rc.local"
-  sudo sh -c "echo 'service gpsd start' >> /etc/rc.local"
-  sudo sh -c "echo '' >> /etc/rc.local"
-  sudo sh -c "echo 'exit 0' >> /etc/rc.local"
+  sudo sh -c "tee -a /etc/rc.local << END
+#!/bin/bash
+
+# Start pps-gpio-modprobe module.
+modprobe pps-gpio-modprobe gpio=${GPIO_PIN}
+# Setting GPS UART
+service gpsd stop
+stty -F ${DEVICE} ${BAUD}
+service gpsd start
+
+exit 0
+END"
   sudo chmod +x /etc/rc.local
 fi
 
@@ -70,10 +70,12 @@ sudo apt install chrony -y
 echo "Do you wish to append a new PPS refclock to /etc/chrony/chrony.conf? [y or Y to accept]"
 read append_chrony_conf
 if [[ $append_chrony_conf == "Y" || $append_chrony_conf == "y" ]]; then
-  sudo sh -c "echo '' >> /etc/chrony/chrony.conf"
-  sudo sh -c "echo '# GPS + PPS' >> /etc/chrony/chrony.conf"
-  sudo sh -c "echo 'refclock PPS /dev/pps0 lock NMEA' >> /etc/chrony/chrony.conf"
-  sudo sh -c "echo 'refclock SHM 0 delay 0.2 refid NMEA' >> /etc/chrony/chrony.conf"
+  sudo sh -c "tee -a /etc/chrony/chrony.conf << END
+
+# GPS + PPS
+refclock PPS /dev/pps0 lock NMEA
+refclock SHM 0 delay 0.2 refid NMEA
+END"
 fi
 
 # Install PPS debug tools.
