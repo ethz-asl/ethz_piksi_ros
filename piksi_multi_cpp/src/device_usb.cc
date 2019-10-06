a15b98127bdeb4e2163e582d6d2d8c7b8a38bcb3
Rik Bähnemann <brik@ethz.ch>
2019-10-06 19:14:53 +0200

    Streamline opening process.

--- piksi_multi_cpp/src/device_usb.cc
+++ piksi_multi_cpp/src/device_usb.cc
@@ -13,6 +13,7 @@ bool DeviceUSB::open() {
   int init = libusb_init(nullptr);
   if (init < 0) {
     ROS_ERROR_STREAM("Cannot initialize libusb: " << init);
+    close();
     return false;
   }
 
@@ -21,52 +22,53 @@ bool DeviceUSB::open() {
   ssize_t cnt = libusb_get_device_list(nullptr, &list);
   if (cnt < 0) {
     ROS_ERROR_STREAM("Invalid device list: " << cnt);
+    close();
     return false;
   }
 
-  // Identify Piksis on USB.
-  std::vector<libusb_device*> piksis;
+  bool new_dev = false;
   for (ssize_t i = 0; i < cnt; ++i) {
+    // Identify Piksi.
     libusb_device* device = list[i];
-    if (identifyPiksi(device)) {
-      piksis.push_back(device);
-    }
-  }
-  if (piksis.empty()) {
-    ROS_INFO("No Piksi discovered on USB.");
-    libusb_free_device_list(list, 1);
-    return false;
-  }
-  ROS_INFO_STREAM("Discovered " << piksis.size() << " Piksi(s) on USB.");
+    if (!identifyPiksi(device)) continue;
 
-  // Open devices.
-  for (auto piksi : piksis) {
-    libusb_device_handle* handle;
-    int open = libusb_open(piksi, &handle);
+    // Open port.
+    int open = libusb_open(device, &handle_);
     if (open < 0) {
       ROS_INFO(
           "Failed to open Piksi Multi on bus number / address %03d.%03d: %d",
-          libusb_get_bus_number(piksi), libusb_get_device_address(piksi), open);
+          libusb_get_bus_number(device), libusb_get_device_address(device),
+          open);
       continue;
     }
-    int detach_kernel = libusb_set_auto_detach_kernel_driver(handle, 1);
-    if (detach_kernel < 0){
+
+    // Claim device.
+    int detach_kernel = libusb_set_auto_detach_kernel_driver(handle_, 1);
+    if (detach_kernel < 0) {
       ROS_WARN_STREAM("Cannot detach kernel driver: " << detach_kernel);
     }
-    int claim = libusb_claim_interface(handle, kInterfaceNumber);
+    int claim = libusb_claim_interface(handle_, kInterfaceNumber);
     if (claim < 0) {
       ROS_INFO_STREAM("Failed to claim device: " << claim);
+      ROS_INFO_COND(claim == LIBUSB_ERROR_BUSY, "Device already claimed.");
       continue;
     }
-    ROS_INFO_STREAM("New device "
-                    << ":");
-    printDeviceInfo(handle);
-    handle_ = handle;
+
+    // Save device handle.
+    ROS_INFO_STREAM("Opened new device:");
+    new_dev = true;
+    printDeviceInfo(handle_);
     break;
   }
 
   libusb_free_device_list(list, 1);
-  return handle_ != nullptr;
+
+  if (new_dev) {
+    return true;
+  } else {
+    close();
+    return false;
+  }
 }
 
 bool DeviceUSB::read() {
@@ -76,12 +78,14 @@ bool DeviceUSB::read() {
 
 bool DeviceUSB::close() {
   bool success = true;
-  int release = libusb_release_interface(handle_, kInterfaceNumber);
-  if (release < 0) {
-    ROS_WARN_STREAM("Device was not released: " << release);
-    success = false;
+  if (handle_) {
+    int release = libusb_release_interface(handle_, kInterfaceNumber);
+    if (release < 0) {
+      ROS_WARN_STREAM("Device was not released: " << release);
+      success = false;
+    }
+    libusb_close(handle_);
   }
-  libusb_close(handle_);
   libusb_exit(nullptr);
   return success;
 }
