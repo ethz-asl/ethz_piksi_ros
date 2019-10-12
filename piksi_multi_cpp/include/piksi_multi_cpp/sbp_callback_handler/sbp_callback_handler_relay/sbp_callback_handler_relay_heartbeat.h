dd4c7429e93a97edfdad9656e614895e038b2d6e
Rik Bähnemann <brik@ethz.ch>
2019-10-12 12:19:51 +0200

    Inline relay callbacks.

--- piksi_multi_cpp/include/piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/sbp_callback_handler_relay_heartbeat.h
+++ piksi_multi_cpp/include/piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/sbp_callback_handler_relay_heartbeat.h
@@ -11,14 +11,28 @@ class SBPCallbackHandlerRelayHeartbeat
     : public SBPCallbackHandlerRelay<piksi_rtk_msgs::Heartbeat,
                                      msg_heartbeat_t> {
  public:
-  SBPCallbackHandlerRelayHeartbeat(const ros::NodeHandle& nh,
-                                   const uint16_t sbp_msg_type,
-                                   const std::shared_ptr<sbp_state_t>& state);
+  inline SBPCallbackHandlerRelayHeartbeat(
+      const ros::NodeHandle& nh, const uint16_t sbp_msg_type,
+      const std::shared_ptr<sbp_state_t>& state)
+      : SBPCallbackHandlerRelay(nh, sbp_msg_type, state, "heartbeat") {}
 
  private:
   // Specialize the message conversion.
-  piksi_rtk_msgs::Heartbeat convertSBPMsgToROSMsg(
-      const msg_heartbeat_t& sbp_msg) override;
+  inline piksi_rtk_msgs::Heartbeat convertSBPMsgToROSMsg(
+      const msg_heartbeat_t& sbp_msg) override {
+    piksi_rtk_msgs::Heartbeat ros_msg;
+    ros_msg.header.stamp = ros::Time::now();
+
+    // How to shift and mask bits: https://stackoverflow.com/a/27592777
+    ros_msg.system_error = (sbp_msg.flags >> 0) & 0x1;
+    ros_msg.io_error = (sbp_msg.flags >> 1) & 0x1;
+    ros_msg.swift_nap_error = (sbp_msg.flags >> 2) & 0x1;
+    ros_msg.sbp_minor_version = (sbp_msg.flags >> 8) & 0xFF;
+    ros_msg.sbp_major_version = (sbp_msg.flags >> 16) & 0xFF;
+    ros_msg.external_antenna_present = (sbp_msg.flags >> 31) & 0x1;
+
+    return ros_msg;
+  }
 };
 
 }  // namespace piksi_multi_cpp
--- piksi_multi_cpp/include/piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/sbp_callback_handler_relay_imu_raw.h
+++ piksi_multi_cpp/include/piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/sbp_callback_handler_relay_imu_raw.h
@@ -7,18 +7,34 @@
 
 namespace piksi_multi_cpp {
 
+// Specialize callback for piksi_rtk_msgs::ImuRawMulti ROS msg type and
+// msg_imu_raw_t SBP msg type.
 class SBPCallbackHandlerRelayImuRaw
     : public SBPCallbackHandlerRelay<piksi_rtk_msgs::ImuRawMulti,
                                      msg_imu_raw_t> {
  public:
-  SBPCallbackHandlerRelayImuRaw(const ros::NodeHandle& nh,
-                                const uint16_t sbp_msg_type,
-                                const std::shared_ptr<sbp_state_t>& state);
+  // Here we define the topic name.
+  inline SBPCallbackHandlerRelayImuRaw(
+      const ros::NodeHandle& nh, const uint16_t sbp_msg_type,
+      const std::shared_ptr<sbp_state_t>& state)
+      : SBPCallbackHandlerRelay(nh, sbp_msg_type, state, "imu_raw") {}
 
  private:
   // Specialize the message conversion.
-  piksi_rtk_msgs::ImuRawMulti convertSBPMsgToROSMsg(
-      const msg_imu_raw_t& sbp_msg) override;
+  inline piksi_rtk_msgs::ImuRawMulti convertSBPMsgToROSMsg(
+      const msg_imu_raw_t& sbp_msg) override {
+    piksi_rtk_msgs::ImuRawMulti ros_msg;
+    ros_msg.tow = sbp_msg.tow;
+    ros_msg.tow_f = sbp_msg.tow_f;
+    ros_msg.acc_x = sbp_msg.acc_x;
+    ros_msg.acc_y = sbp_msg.acc_y;
+    ros_msg.acc_z = sbp_msg.acc_z;
+    ros_msg.gyr_x = sbp_msg.gyr_x;
+    ros_msg.gyr_y = sbp_msg.gyr_y;
+    ros_msg.gyr_z = sbp_msg.gyr_z;
+
+    return ros_msg;
+  }
 };
 
 }  // namespace piksi_multi_cpp
