4087edd9d5d7a98ab7f9a444d93c95913841b50f
Rik Bähnemann <brik@ethz.ch>
2019-11-11 18:33:20 +0100

    More sceleton.

--- piksi_multi_cpp/include/piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/ros_enu_relays.h
+++ piksi_multi_cpp/include/piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/ros_enu_relays.h
@@ -1,23 +1,58 @@
 #ifndef PIKSI_MULTI_CPP_SBP_CALLBACK_HANDLER_SBP_CALLBACK_HANDLER_RELAY_ROS_ENU_RELAYS_H_
 #define PIKSI_MULTI_CPP_SBP_CALLBACK_HANDLER_SBP_CALLBACK_HANDLER_RELAY_ROS_ENU_RELAYS_H_
 
+#include <geometry_msgs/PointStamped.h>
+#include <geotf/geodetic_converter.h>
 #include <libsbp/navigation.h>
 #include <piksi_rtk_msgs/PositionWithCovarianceStamped.h>
+#include <Eigen/Dense>
+#include <optional>
 #include "piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/ros_relay.h"
 
 namespace piksi_multi_cpp {
 
+// Per default the ENU origin will be the base station position. But it can also
+// be set to the current rover position or a user defined position.
+template <class SbpMsgType, class RosMsgType>
+class RosEnuRelay : public RosRelay<SbpMsgType, RosMsgType> {
+ public:
+  inline RosEnuRelay(const ros::NodeHandle& nh, const uint16_t sbp_msg_type,
+                     const std::shared_ptr<sbp_state_t>& state,
+                     const std::string& topic,
+                     const RosTimeHandler::Ptr& ros_time_handler)
+      : RosRelay<SbpMsgType, RosMsgType>(nh, sbp_msg_type, state, topic,
+                                         ros_time_handler, "enu") {
+    geotf_.addFrameByEPSG("ecef", 4978);
+    geotf_.addFrameByEPSG("wgs84", 4326);
+  }
+
+ public:
+  inline void setEnuOrigin(const double lat, const double lon,
+                           const double alt) {
+    enu_origin_wgs84_ = Eigen::Vector3d(lat, lon, alt);
+  }
+
+  inline void resetEnuOrigin() { enu_origin_wgs84_.reset(); }
+
+ protected:
+  bool use_base_enu_origin_ = true;  // False: ENU origin is first position.
+  std::optional<Eigen::Vector3d> enu_origin_wgs84_;
+  geotf::GeodeticConverter geotf_;
+
+ private:
+};
+
 class RosPosEnuRelay
-    : public RosRelay<msg_pos_ecef_cov_t,
-                      piksi_rtk_msgs::PositionWithCovarianceStamped> {
+    : public RosEnuRelay<msg_pos_ecef_t, geometry_msgs::PointStamped> {
  public:
   inline RosPosEnuRelay(const ros::NodeHandle& nh,
                         const std::shared_ptr<sbp_state_t>& state,
                         const RosTimeHandler::Ptr& ros_time_handler)
-      : RosRelay(nh, SBP_MSG_POS_ECEF, state, "pos_enu", ros_time_handler,
-                 "enu") {}
+      : RosEnuRelay(nh, SBP_MSG_POS_ECEF, state, "pos_enu", ros_time_handler) {}
 
  private:
+  void convertSbpMsgToRosMsg(const msg_pos_ecef_t& in, const uint8_t len,
+                             geometry_msgs::PointStamped* out) override;
 };
 
 }  // namespace piksi_multi_cpp
--- piksi_multi_cpp/src/sbp_callback_handler/sbp_callback_handler_relay/ros_enu_relays.cc
+++ piksi_multi_cpp/src/sbp_callback_handler/sbp_callback_handler_relay/ros_enu_relays.cc
@@ -1,3 +1,18 @@
+#include <libsbp_ros_msgs/ros_conversion.h>
 #include "piksi_multi_cpp/sbp_callback_handler/sbp_callback_handler_relay/ros_enu_relays.h"
 
-namespace piksi_multi_cpp {}
+namespace piksi_multi_cpp {
+
+namespace lrm = libsbp_ros_msgs;
+
+void RosPosEnuRelay::convertSbpMsgToRosMsg(const msg_pos_ecef_t& in,
+                                           const uint8_t len,
+                                           geometry_msgs::PointStamped* out) {
+  ROS_ASSERT(out);
+
+  // Convert measurement to Eigen.
+  Eigen::Vector3d x_ecef;
+  lrm::convertCartesianPoint<msg_pos_ecef_t>(in, &x_ecef);
+}
+
+}  // namespace piksi_multi_cpp
