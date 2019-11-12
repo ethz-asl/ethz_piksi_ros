3e07705211f744a6d84c900e65f419c0ff9d99ef
Rik Bähnemann <brik@ethz.ch>
2019-11-12 21:05:27 +0100

    Add installation scripts.

---
+++ piksi_multi_cpp/install/prepare-jenkins-slave.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+echo "Running the prepare script for piksi_multi_cpp.";
+ROS_VERSION=`rosversion -d`
+echo "ROS version: ${ROS_VERSION}"
+
+# Build dependencies.
+sudo apt-get install -y python-wstool python-catkin-tools
+
+# Package dependencies.
+echo "Installing libsbp_ros_msgs dependencies."
+sudo apt install -y setuptools jinja2
