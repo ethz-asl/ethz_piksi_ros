c232300435984b47c740096ab0b2614b5eb56979
Rik Bähnemann <brik@ethz.ch>
2019-11-13 01:37:54 +0100

    Add more dependencies.

--- piksi_multi_cpp/install/prepare-jenkins-slave.sh
+++ piksi_multi_cpp/install/prepare-jenkins-slave.sh
@@ -11,3 +11,10 @@ echo "Installing libsbp_ros_msgs dependencies."
 sudo apt install -y python-pip
 pip install jinja2
 pip install setuptools
+
+echo "Installing geodetic_utils and geotf dependencies."
+sudo apt install ros-${ROS_VERSION}-tf-conversions -y
+sudo apt install libgdal-dev -y
+
+echo "Installing rqt_gps_rtk_plugin dependencies."
+sudo apt install ros-${ROS_VERSION}-rqt-gui ros-${ROS_VERSION}-rqt-gui-cpp qt5-default -y
