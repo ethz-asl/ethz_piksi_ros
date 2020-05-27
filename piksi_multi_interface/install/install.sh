aa3395ac0cd41c20881bfd9867b14cc69629c228
Rik Bähnemann <brik@ethz.ch>
2020-05-27 15:44:05 +0000

    Refine autostart

--- piksi_multi_interface/install/install.sh
+++ piksi_multi_interface/install/install.sh
@@ -2,15 +2,15 @@
 
 echo "Setup automatic interface startup."
 
-echo "Please enter the PPS gpiochip, e.g., gpiochip3..."
+echo "Please enter the survey gpiochip, e.g., gpiochip3..."
 read GPIOCHIP
+
+echo "Please enter the survey gpio offset, e.g., 0..."
+read OFFSET
 
 echo "Do you wish to configure UDEV rule for the gpiochip and add user to group gpio? [y or Y to accept]"
 read create_udev
 if [[ $create_udev == "Y" || $create_udev == "y" ]]; then
-  echo "Please enter the PPS gpiochip, e.g., gpiochip3..."
-  read GPIOCHIP
-
   sudo rm /etc/udev/rules.d/99-gpio.rules
   sudo sh -c "tee -a /etc/udev/rules.d/99-gpio.rules << END
 KERNEL==\"${GPIOCHIP}\", GROUP=\"gpio\"
@@ -33,7 +33,7 @@ After=piksi.service
 
 [Service]
 Type=forking
-ExecStart=/home/$USER/catkin_ws/src/ethz_piksi_ros/piksi_multi_interface/install/startup_interface.sh
+ExecStart=/home/$USER/catkin_ws/src/ethz_piksi_ros/piksi_multi_interface/install/startup_interface.sh $GPIOCHIP $OFFSET
 User=$USER
 
 [Install]
