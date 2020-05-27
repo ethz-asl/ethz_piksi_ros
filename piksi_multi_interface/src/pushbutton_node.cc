222e0c24a88a07c9ac8a3964ea81a6094d820992
Rik Bähnemann <brik@ethz.ch>
2020-05-27 09:42:43 +0200

    Interface GPIO.

--- piksi_multi_interface/src/pushbutton_node.cc
+++ piksi_multi_interface/src/pushbutton_node.cc
@@ -7,15 +7,29 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "piksi_pushbutton");
   ros::NodeHandle nh("~");
 
+  // ROS parameters
   int rate = 1000;
   nh.getParam("rate", rate);
+
+  std::string chip = "gpiochip0";
+  nh.getParam("chip", rate);
+
+  int line = 0;
+  nh.getParam("line", line);
 
   ros::Publisher status_pub = nh.advertise<std_msgs::Bool>("status", 1);
 
   ros::Rate loop_rate(rate);
   while (ros::ok()) {
     std_msgs::Bool status;
-    status.data = false;
+    auto ret = gpiod_ctxless_get_value(chip.c_str(), line, false,
+                                       ros::this_node::getName().c_str());
+    if (ret < 0) {
+      ROS_ERROR("Cannot get GPIO value on chip %s line %d", chip.c_str(), line);
+      return 0;
+    }
+
+    status.data = ret;
     status_pub.publish(status);
 
     ros::spinOnce();
