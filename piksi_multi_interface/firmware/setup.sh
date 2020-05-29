8c2d59b0cfc1356a8a10d7c20a31f9e544d8ea98
Rik Bähnemann <brik@ethz.ch>
2020-05-29 12:05:57 +0200

    Fix sketchbook path.

--- piksi_multi_interface/firmware/setup.sh
+++ piksi_multi_interface/firmware/setup.sh
@@ -2,7 +2,8 @@ source ~/catkin_ws/devel/setup.bash
 rm -rf libraries/ros_lib
 rosrun rosserial_arduino make_libraries.py libraries
 
-arduino --pref "sketchbook.path=/home/$HOSTNAME/catkin_ws/src/ethz_piksi_ros/piksi_multi_interface/firmware"
+SKETCHBOOK_PATH=/home/$USER/catkin_ws/src/ethz_piksi_ros/piksi_multi_interface/firmware
+
 arduino --install-boards arduino:avr
-arduino --install-library Adafruit\ NeoPixel:1.4.0
-arduino --upload --board arduino:avr:micro --port /dev/ttyACM0 status_led/status_led.ino
+arduino --pref "sketchbook.path=$SKETCHBOOK_PATH" --install-library Adafruit\ NeoPixel:1.4.0
+arduino --pref "sketchbook.path=$SKETCHBOOK_PATH" --upload --board arduino:avr:micro --port /dev/ttyACM0 status_led/status_led.ino
