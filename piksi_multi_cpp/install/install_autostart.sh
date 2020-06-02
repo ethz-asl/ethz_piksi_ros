acf1c7b521fbde91ed03d897352eee0526136cb2
Rik Bähnemann <brik@ethz.ch>
2020-06-02 21:45:20 +0200

    Robustify Piksi startup.

--- piksi_multi_cpp/install/install_autostart.sh
+++ piksi_multi_cpp/install/install_autostart.sh
@@ -19,9 +19,11 @@ After=network-online.target
 
 [Service]
 Type=forking
+ExecStartPre=/bin/sleep 120
 ExecStart=/home/$USER/catkin_ws/src/ethz_piksi_ros/piksi_multi_cpp/install/startup_${TYPE}.sh
 Restart=on-failure
 User=$USER
+TimeoutStartSec=121
 
 [Install]
 WantedBy=multi-user.target
