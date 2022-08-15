fba61c21c56e3d5086508cd78d4b4e5598836bc2
Rik Bähnemann <brik@ethz.ch>
2022-08-15 18:50:45 +0200

    Circle.

--- piksi_multi_interface/scripts/neopixel_node.py
+++ piksi_multi_interface/scripts/neopixel_node.py
@@ -5,9 +5,14 @@ import neopixel
 
 def status_led():
     pixels = neopixel.NeoPixel(board.D12, n=7, bpp=4)
-    pixels[1] = (255, 0, 0)
-    pixels[2] = (0, 255, 0)
-    pixels[3] = (0, 0, 255)
+    rate = rospy.Rate(10)
+    idx = 1
+    while not rospy.is_shutdown():
+        pixels[(idx % 7) + 1] = (255, 0, 0)
+        pixels[(idx + 1 % 7) + 1] = (0, 255, 0)
+        pixels[(idx + 2 % 7) + 1] = (0, 0, 255)
+        idx = idx + 1
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
