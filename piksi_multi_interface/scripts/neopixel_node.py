259636601c9f7a87ba6a390b2fe65761672dce04
Rik Bähnemann <brik@ethz.ch>
2022-08-15 18:55:47 +0200

    Indices.

--- piksi_multi_interface/scripts/neopixel_node.py
+++ piksi_multi_interface/scripts/neopixel_node.py
@@ -7,11 +7,12 @@ def status_led():
     pixels = neopixel.NeoPixel(board.D12, n=7, bpp=4)
     rospy.init_node('status_led', anonymous=True)
     rate = rospy.Rate(10)
-    idx = 1
+    idx = 0
     while not rospy.is_shutdown():
-        pixels[(idx % 7) + 1] = (255, 0, 0)
-        pixels[(idx + 1 % 7) + 1] = (0, 255, 0)
-        pixels[(idx + 2 % 7) + 1] = (0, 0, 255)
+        idx = (idx % 7)
+        pixels[idx + 1] = (255, 0, 0)
+        pixels[idx + 2] = (0, 255, 0)
+        pixels[idx + 3) + 1] = (0, 0, 255)
         idx = idx + 1
         rate.sleep()
 
