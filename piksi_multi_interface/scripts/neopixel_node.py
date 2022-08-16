8809f4b57042a9866b532c9dd3082f530e6e0152
Rik Bähnemann <brik@ethz.ch>
2022-08-16 15:04:06 +0000

    Finalize status LEDs.

--- piksi_multi_interface/scripts/neopixel_node.py
+++ piksi_multi_interface/scripts/neopixel_node.py
@@ -3,13 +3,13 @@ import rospy
 import board
 import neopixel
 
-from libsbp_ros_msgs.msg import MsgHeartbeat
+from libsbp_ros_msgs.msg import MsgHeartbeat, MsgPosEcef, MsgDgnssStatus
 
 # Neopixel definitions
 GPIO=board.D12
 NUM_PIXELS=7
 ORDER=neopixel.GRBW
-BRIGHTNESS=0.1
+BRIGHTNESS=1.0
 pixels = neopixel.NeoPixel(GPIO, n=NUM_PIXELS, pixel_order=ORDER)
 
 NONE=(0, 0, 0, 0)
@@ -35,11 +35,14 @@ FIX_SBAS=6
 LEDS_PER_SAT=3
 TOGGLE_HZ=2
 
-color_solution=RED
+# Global variables
+color_solution=NONE
+color_corrections=NONE
 blinking_solution=False
 has_heartbeat=False
 first_pixel_rainbow=0
 toggle=True
+num_sats = 0
 
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
@@ -76,34 +79,53 @@ def heartbeat_cb(msg):
     has_heartbeat = True
     rospy.loginfo_once("Received first heartbeat.")
 
-def gnss_cb(msg):
-    global blinking_solution, color_solution
+def pos_ecef_cb(msg):
+    global blinking_solution, color_solution, num_sats
 
     fix_mode = (msg.flags >> 0) & 0x7
     if fix_mode == FIX_INVALID:
         color_solution = RED
         blinking_solution = True
+        rospy.logwarn_throttle(THROTTLE_PERIOD, "Invalid solution. Antenna missing?")
     elif fix_mode == FIX_SPP:
         color_solution = ORANGE
         blinking_solution = False
+        rospy.logdebug("FIX_SPP solution.")
     elif fix_mode == FIX_DGNSS:
         color_solution = PURPLE
         blinking_solution = False
+        rospy.logdebug("FIX_DGNSS solution.")
     elif fix_mode == FIX_FLOAT_RTK:
         color_solution = BLUE
         blinking_solution = True
+        rospy.logdebug("FIX_FLOAT_RTK solution.")
     elif fix_mode == FIX_FIXED_RTK:
         color_solution = BLUE
         blinking_solution = False
+        rospy.logdebug("FIX_FIXED_RTK solution.")
     elif fix_mode == FIX_DEAD_RECKONING:
         color_solution = ORANGE
         blinking_solution = True
+        rospy.logdebug("FIX_DEAD_RECKONING solution.")
     elif fix_mode == FIX_SBAS:
         color_solution = GREEN
         blinking_solution = False
+        rospy.logdebug("FIX_SBAS solution.")
     else:
         color_solution = RED
         blinking_solution = False
+        rospy.logerror("Unknown FIX solution.")
+
+    num_sats = msg.n_sats
+
+def dgnss_status_cb(msg):
+    global color_corrections
+
+    dgnss_type = (msg.flags >> 0) & 0xF
+    if dgnss_type != 0:
+        color_corrections = RED
+    else:
+        color_corrections = NONE
 
 def status_led():
     global first_pixel_rainbow, toggle
@@ -112,24 +134,38 @@ def status_led():
     pixels.brightness=BRIGHTNESS
 
     rospy.Subscriber("/piksi_multi_cpp_base/base_station_receiver_0/sbp/heartbeat", MsgHeartbeat, heartbeat_cb)
+    rospy.Subscriber("/piksi_multi_cpp_base/base_station_receiver_0/sbp/pos_ecef", MsgPosEcef, pos_ecef_cb)
+    rospy.Subscriber("/piksi_multi_cpp_base/base_station_receiver_0/sbp/dgnss_status", MsgDgnssStatus, dgnss_status_cb)
 
     while not rospy.is_shutdown():
         if not has_heartbeat:
+            # Wait for first heartbeat
             rainbow(first_pixel_rainbow)
             first_pixel_rainbow = (first_pixel_rainbow + 1) % 255
             rospy.Rate(FLASH_HZ).sleep()
             rospy.logwarn_throttle(THROTTLE_PERIOD, "No Piksi heartbeat.")
         else:
-            # Color ring and center according to solution 
-            pixels.fill(NONE)
+            # Color ring according to solution
             for i in range(1,NUM_PIXELS):
-                pixels[i] = color_solution
+                if color_solution == RED or num_sats >= i * LEDS_PER_SAT:
+                    pixels[i] = color_solution
+                else:
+                    pixels[i] = NONE
             if blinking_solution and toggle:
-                pixels.fill(NONE)
-                toggle = not toggle
+                for i in range(1, NUM_PIXELS):
+                    pixels[i] = NONE
+
+            # Color center according to corrections
+            if toggle:
+            	pixels[0] = color_corrections
+            else:
+                pixels[0] = NONE
+
+            toggle = not toggle
             rospy.Rate(TOGGLE_HZ).sleep()
 
-
+    # Turn off LEDs at termination.
+    pixels.fill(NONE)
 if __name__ == '__main__':
     try:
         status_led()
