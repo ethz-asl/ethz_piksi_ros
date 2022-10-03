1277d77316f97cff71f99c0c325824ec4a8931bd
Rik Bähnemann <brik@ethz.ch>
2022-10-03 13:02:33 +0200

    Fix geographic coordinates.

--- libsbp_ros_msgs/test/geo-conversion-test.cpp
+++ libsbp_ros_msgs/test/geo-conversion-test.cpp
@@ -36,23 +36,20 @@ TEST(GeoConversionTest, SampleGnssPoints) {
   // A couple of sample points logged with Piksi CPP driver.
   // The ENU position is derived from base_pos_ned
   const std::vector<Eigen::Vector3d> enu_measured = {
-      {10.863, 2.435, 1.514},
-      {10.703000, 2.787000, 8.211000},
-      {10.811000, 3.065000, 17.840000},
-      {10.841000, 3.162000, 20.986000},
-      {11.702000, -20.480000, 41.105000}};
+      {10.857000000000001, 2.353, 0.198},
+      {10.036, -17.792, 41.247},
+      {6.317, 5.981, 41.232},
+      {2.852, 39.489000000000004, 41.238}};
   const std::vector<Eigen::Vector3d> wgs84_measured = {
-      {47.381494, 8.576296, 648.879785},
-      {47.381498, 8.576294, 655.576665},
-      {47.381500, 8.576295, 665.205155},
-      {47.381501, 8.576295, 668.351331},
-      {47.381288, 8.576307, 688.470171}};
+      {47.38149370420578, 8.57629557889748, 647.5638267526044},
+      {47.38131252854044, 8.576284714398815, 688.6127781794771},
+      {47.38152633220841, 8.576235472927848, 688.5978165181814},
+      {47.381827687678694, 8.576189587067374, 688.6035694238228}};
   const std::vector<Eigen::Vector3d> ecef_measured = {
-      {4278628.531598, 645271.204977, 4671064.044718},
-      {4278632.782633, 645271.684303, 4671069.211647},
-      {4278639.011154, 645272.732183, 4671076.485160},
-      {4278641.042840, 645273.068989, 4671078.865808},
-      {4278671.587614, 645278.546191, 4671077.662780}};
+      {4278627.710932162, 645271.074246071, 4671063.021060776},
+      {4278669.975769749, 645276.6185371446, 4671079.587635878},
+      {4278653.221991419, 645270.3310393207, 4671095.673600099},
+      {4278629.360581043, 645263.227948546, 4671118.366348593}};
 
   for (size_t i = 0; i < enu_measured.size(); i++) {
     // Test WGS84 <-> ENU
@@ -60,8 +57,9 @@ TEST(GeoConversionTest, SampleGnssPoints) {
     // WGS84 -> ENU
     EXPECT_TRUE(geo_conv.convertWgs84ToEnu(wgs84_measured[i], &enu_from_wgs84));
     EXPECT_TRUE(enu_from_wgs84.isApprox(enu_measured[i], kEnuRes))
-        << "enu_from_wgs84: " << enu_from_wgs84.transpose() << " enu_measured[" << i
-        << "]: " << enu_measured[i].transpose();
+        << "wgs84_measured[" << i << "]: " << wgs84_measured[i].transpose()
+        << " enu_from_wgs84: " << enu_from_wgs84.transpose() << " enu_measured["
+        << i << "]: " << enu_measured[i].transpose();
     EXPECT_TRUE(geo_conv.convertEnuToWgs84(enu_from_wgs84, &wgs84_from_enu));
     EXPECT_TRUE(wgs84_from_enu.isApprox(wgs84_measured[i], kEnuRes));
     // ENU -> WGS84
